4b0afd14ae285a16e41010ebddea09ca5ceb187e
Gossler Karoly <connor@connor.hu>
2016-03-12 11:59:36 +0100

    cleanup

--- _refs.d.ts
+++
@@ -1,6 +0,0 @@
-﻿///<reference path="typings/asyncawait/asyncawait.d.ts" />
-///<reference path="typings/bluebird/bluebird.d.ts" />
-///<reference path="typings/lodash/lodash.d.ts" />
-///<reference path="typings/moment/moment.d.ts" />
-///<reference path="typings/node/node.d.ts" />
-declare module "_refs" { }
