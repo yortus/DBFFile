0dc0a38e4691f2f28f4d2d568fdf64ede9a7052e
Troy Gerwien <yortus@gmail.com>
2016-03-14 14:59:24 +0800

    restore writing test

--- test/writing-a-dbf-file.ts
+++ test/writing-a-dbf-file.ts
@@ -1,34 +1,41 @@
-// 'use strict';
-// import * as path from 'path';
-// import * as _ from 'lodash';
-// import * as Promise from 'bluebird';
-// var rimraf = Promise.promisify(require('rimraf'));
-// import {async, await} from 'asyncawait';
-// import {expect} from 'chai';
-// import * as DBFFile from 'dbffile';
+'use strict';
+import * as path from 'path';
+import * as _ from 'lodash';
+import * as Promise from 'bluebird';
+var rimraf = Promise.promisify(require('rimraf'));
+import {async, await} from 'asyncawait';
+import {expect} from 'chai';
+import * as DBFFile from 'dbffile';
 
 
-// describe('Writing a DBF file', () => {
+describe('Writing a DBF file', () => {
 
-//     let tests = [
-//         'dbase_03.dbf'
-//     ];
+    let tests = [
+        {
+            filename: 'PYACFL.DBF',
+            rowCount: 15,
+            firstRow: { AFCLPD: 'W', AFHRPW: 2.92308, AFLVCL: 0.00, AFCRDA: new Date(1999, 2, 25), AFPSDS: '' }
+        },
+    ];
 
-//     rimraf(path.join(__dirname, `./fixtures/*.out`));
+    rimraf(path.join(__dirname, `./fixtures/*.out`));
 
-//     tests.forEach(test => {
-//         it(test, async.cps (() => {
-//             let srcPath = path.join(__dirname, `./fixtures/${test}`);
-//             let dstPath = path.join(__dirname, `./fixtures/${test}.out`);
+    tests.forEach(test => {
+        it(test.filename, async.cps (() => {
+            let srcPath = path.join(__dirname, `./fixtures/${test.filename}`);
+            let dstPath = path.join(__dirname, `./fixtures/${test.filename}.out`);
 
-//             let srcDbf = await (DBFFile.open(srcPath));
-//             let tgtDbf = await (DBFFile.create(dstPath, srcDbf.fields));
+            let srcDbf = await (DBFFile.open(srcPath));
+            let dstDbf = await (DBFFile.create(dstPath, srcDbf.fields));
 
-//             let rows = await (srcDbf.readRecords(100));
+            let rows = await (srcDbf.readRecords(100));
+            await (dstDbf.append(rows));
 
-//             await (tgtDbf.append(rows));
-
-//             // TODO: add assertions...            
-//         }));
-//     });
-// });
+            dstDbf = await (DBFFile.open(dstPath));
+            rows = await (dstDbf.readRecords(500));
+            let firstRow = _.pick(rows[0], _.keys(test.firstRow));
+            expect(dstDbf.recordCount).equal(test.rowCount);
+            expect(firstRow).deep.equal(test.firstRow);
+        }));
+    });
+});
