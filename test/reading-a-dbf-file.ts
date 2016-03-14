aa2c2889029cb3e8a1ef767c55db7d9410c38d74
Troy Gerwien <yortus@gmail.com>
2016-03-14 14:48:30 +0800

    add test fixture (other one has dupl. field name)

--- test/reading-a-dbf-file.ts
+++ test/reading-a-dbf-file.ts
@@ -10,9 +10,16 @@ describe('Reading a DBF file', () => {
 
     let tests = [
         {
+            filename: 'PYACFL.DBF',
+            rowCount: 15,
+            firstRow: { AFCLPD: 'W', AFHRPW: 2.92308, AFLVCL: 0.00, AFCRDA: new Date(1999, 3, 25), AFPSDS: '' },
+            error: null
+        },
+        {
             filename: 'dbase_03.dbf',
-            rowCount: 14,
-            firstRow: { Point_ID: '0507121', Shape: 'circular', Std_Dev: 0.897088, GPS_Date: new Date(2005, 6, 12) }
+            rowCount: null,
+            firstRow: null,
+            error: `Duplicate field name: 'Point_ID'`
         }
     ];
 
@@ -21,17 +28,26 @@ describe('Reading a DBF file', () => {
             let filepath = path.join(__dirname, `./fixtures/${test.filename}`);
             let expectedRows = test.rowCount;
             let expectedData = test.firstRow;
+            let expectedError = test.error;
             let actualRows = null;
             let actualData = null;
+            let actualError = null;
             try {
                 let dbf = await (DBFFile.open(filepath));
-                let rows = await (dbf.readRecords(1));
+                let rows = await (dbf.readRecords(100));
                 actualRows = dbf.recordCount;
                 actualData = _.pick(rows[0], _.keys(expectedData));
             }
-            catch (ex) { }
-            expect(actualRows).equals(expectedRows);
-            expect(actualData).deep.equal(expectedData);
+            catch (ex) {
+                actualError = ex.message;
+            }
+            if (expectedError || actualError) {
+                expect(actualError).equals(expectedError);
+            }
+            else {
+                expect(actualRows).equals(expectedRows);
+                expect(actualData).deep.equal(expectedData);
+            }
         }));
     });
 });
