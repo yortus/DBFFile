d583c722021a2cc1b72e824bee75eff263498dc0
yortus <yortus@gmail.com>
2019-06-06 13:42:52 +0800

    /built --> /dist

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,7 +1,7 @@
 
 
 // Tell mocha where the test files are.
-process.argv.push('built/test/**/*.js');
+process.argv.push('dist/test/**/*.js');
 
 // Tell mocha *not* to call process.exit() when tests have finished.
 process.argv.push('--no-exit');
