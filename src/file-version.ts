51a81bad352a3dbf66b480f516589478feda508f
Anbraten <anton@ju60.de>
2020-10-08 08:14:37 +0200

    allow dbf files with version 0x31

--- src/file-version.ts
+++ src/file-version.ts
@@ -3,11 +3,12 @@ export type FileVersion =
     | 0x83 // dBase III with memo file
     | 0x8b // dBase IV with memo file
     | 0x30 // Visual FoxPro 9 without memo file
+    | 0x31
 ;
 
 
 
 
 export function isValidFileVersion(fileVersion: number): fileVersion is FileVersion {
-    return [0x03, 0x83, 0x8b, 0x30].includes(fileVersion);
+    return [0x03, 0x83, 0x8b, 0x30, 0x31].includes(fileVersion);
 }
