2763f7cfbb1662eacf3524a277742b18b9a2b45e
Khaos66 <khaos@khaos-coders.org>
2021-10-12 10:02:27 +0200

    Allow Memo of size 4

--- src/field-descriptor.ts
+++ src/field-descriptor.ts
@@ -40,9 +40,11 @@ export function validateFieldDescriptor(field: FieldDescriptor, fileVersion: num
     if (type === 'F' && size > 20) throw new Error('Field size is too large (maximum is 20)');
     if (type === 'L' && size !== 1) throw new Error('Invalid field size (must be 1)');
     if (type === 'D' && size !== 8) throw new Error('Invalid field size (must be 8)');
-    if (type === 'M' && size !== 10) throw new Error('Invalid field size (must be 10)');
     if (type === 'T' && size !== 8) throw new Error('Invalid field size (must be 8)');
     if (type === 'B' && size !== 8) throw new Error('Invalid field size (must be 8)');
+
+    const memoSize = fileVersion == 0x30 ? 4 : 10;
+    if (type === 'M' && size !== memoSize) throw new Error(`Invalid field size (must be ${memoSize})`);
 
     // decimalPlaces
     const maxDecimals = fileVersion === 0x8b ? 18 : 15;
